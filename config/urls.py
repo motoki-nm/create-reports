10d8348104a0988a7cf5368fcbc423031dc96a88
nmgr09072-spec <n.m.gr.0907.2@gmail.com>
2026-07-04 09:44:05 +0900

    auto: edit config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from reports.views import register
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
+    path("register/", register, name="register"),
     path("", include("reports.urls")),
     path("expenses/", include("expenses.urls")),
 ]
